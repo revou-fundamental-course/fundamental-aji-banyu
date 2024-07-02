ec6106aaea685552b5d0383d20f1a2d72619e03f
aji-banyu <ajibanyurizqi@gmail.com>
2024-07-02 23:55:18 +0700

    fungsi submit form

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
+// fungsi toggle navbar
 const navbar = document.querySelector(".text-nav");
 // Ketika humberger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbar.classList.toggle("active");
 };
+// klik diluar sidebar untuk menghilangkan hamburger menu
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbar.contains(e.target)) {
+    navbar.classList.remove("active");
+  }
+});
+
+//        fungsi untuk validasi form
+function formValue() {
+  const nama = document.getElementById("nama").value;
+  const pesan = document.getElementById("pesan").value;
+  const tglLahir = document.getElementById("tgl").value;
+  const kelamin = document.querySelector(".kelamin").value;
+
+  document.getElementById("sender-nama").innerText = nama;
+  document.getElementById("sender-tglLahir").innerText = tglLahir;
+  document.getElementById("sender-gender").innerText = kelamin;
+  document.getElementById("sender-pesan").innerText = pesan;
+
+  console.log(nama);
+}
