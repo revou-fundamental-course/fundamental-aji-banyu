c7f3596fe8fc26f4d76eec2eb5e6061a3a3988f2
aji-banyu <ajibanyurizqi@gmail.com>
2024-07-02 12:41:23 +0700

    navigation button

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const navbar = document.querySelector(".text-nav");
+// Ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbar.classList.toggle("active");
+};
